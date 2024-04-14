1ef0a8a77dad4915acd94d423f90b1286329ddc2
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-14 14:20:28 -0600

    chore: configured components autoimport and added format to test folder

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,10 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    App: typeof import('./src/App.vue')['default']
+    HolaMundo: typeof import('./src/modules/test/components/HolaMundo.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TestView: typeof import('./src/modules/test/views/TestView.vue')['default']
   }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TestComponent from '@/modules/test/TestComponent.vue'
+import TestView from '@/modules/test/views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: TestComponent,
+      component: TestView,
     },
   ]
 })
