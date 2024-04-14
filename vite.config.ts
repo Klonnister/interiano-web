2fd3676e2df3b10cdfd9b9988596828d567f673f
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-14 14:10:04 -0600

    chore: added unplugin-components-autoimport

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Components({
+      dts: true,
+      types: [{
+        from: 'vue-router',
+        names: ['RouterLink', 'RouterView'],
+      }],
+      globs: ['src/*.{vue}'],
+      deep: true,
+    }),
     vue(),
     VueDevTools(),
   ],
