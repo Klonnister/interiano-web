979713a94a0951dcdebff9a853f6a8b4aa232ef6
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-15 15:11:27 -0600

    feat: added primevue components and classes

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/md-dark-deeppurple/theme.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 app.use(vuetify)
 app.use(router)
 
