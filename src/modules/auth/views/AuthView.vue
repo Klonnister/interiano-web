e1a571c8eab76f0444eca57e12a95c216077c2b6
Darin Aguilar <bromomentanium@gmail.com>
2024-04-29 14:11:47 -0600

    feat: auth view file created

---
+++ src/modules/auth/views/AuthView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen w-full">
+    <header>
+      <img alt="Interiano logo" src="/interiano-logo-shadowed.svg" />
+    </header>
+
+    <RouterView />
+  </div>
+</template>
