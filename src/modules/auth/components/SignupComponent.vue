b3c48e429bce0524f780d5a462a8edc339bba4a8
Darin Aguilar <bromomentanium@gmail.com>
2024-04-29 14:11:11 -0600

    feat: sign up component file created

---
+++ src/modules/auth/components/SignupComponent.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import type { singUpType } from "@/typings/interfaces/Auth/SingUpType";
+import { reactive } from "vue";
+
+//variables
+const form = reactive<singUpType>({
+  name: "",
+  password: "",
+  passwordConfirm: "",
+});
+
+//submit
+function submit() {
+  console.log(form);
+}
+</script>
+
+<template>
+  <div>
+    <!-- title -->
+    <div>
+      <span>Regístrate</span>
+    </div>
+    <!-- end title -->
+    <!-- form -->
+    <div>
+      <input type="text" name="name" id="name"  v-model="form.name">
+      <input type="password" name="password" id="password" v-model="form.password">
+      <input type="password" name="passwordConfirm" id="passwordConfirm" v-model="form.passwordConfirm">
+      <button type="submit" @click="submit ">Registrarse</button>
+    </div>
+    <!-- end form -->
+    <!-- link buttons -->
+    <div>
+      <button type="submit" >Volver</button>
+    </div>
+     <!-- end link buttons -->
+  </div>
+</template>
