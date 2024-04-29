85281fff6fb0a5f127d65678ba0e48404d7c03a1
Darin Aguilar <bromomentanium@gmail.com>
2024-04-29 14:12:25 -0600

    chore: format exports

--- components.d.ts
+++ components.d.ts
@@ -8,9 +8,12 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     App: typeof import('./src/App.vue')['default']
+    AuthView: typeof import('./src/modules/auth/views/AuthView.vue')['default']
     HolaMundo: typeof import('./src/modules/test/components/HolaMundo.vue')['default']
+    LogInComponent: typeof import('./src/modules/auth/components/LogInComponent.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SignupComponent: typeof import('./src/modules/auth/components/SignupComponent.vue')['default']
     TestView: typeof import('./src/modules/test/views/TestView.vue')['default']
   }
 }
