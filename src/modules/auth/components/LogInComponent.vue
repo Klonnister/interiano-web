d92cd7c237229a4250ca2139410e41afb4404872
Darin Aguilar <bromomentanium@gmail.com>
2024-04-29 14:11:31 -0600

    feat: login component file created

---
+++ src/modules/auth/components/LogInComponent.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { logInType } from "@/typings/interfaces/Auth/LogInType";
+import { reactive } from "vue";
+
+//variables
+const form = reactive<logInType>({
+  name: "",
+  password: "",
+});
+
+//submit
+function submit() {
+  console.log(form);
+}
+</script>
+
+<template>
+  <div>
+    <!-- title -->
+    <div>
+      <span> Inicia sesión </span>
+    </div>
+    <!-- end title -->
+    <!-- form -->
+    <div>
+      <input type="text" name="name" id="name" v-model="form.name" />
+      <input type="password" name="password" id="password" v-model="form.password" />
+      <button type="submit" @click="submit">Add</button>
+    </div>
+    <!-- end form -->
+    <!-- link buttons -->
+    <div>
+      <div>
+        <img src="" alt="" />
+        <span> Registrate </span>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <span>Modo invitado</span>
+      </div>
+    </div>
+    <!-- link buttons -->
+  </div>
+</template>
