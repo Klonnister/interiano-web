55a9d783f1279ca766ce87edf76611660d372320
Darin Aguilar <bromomentanium@gmail.com>
2024-04-29 14:45:45 -0600

    feat: button card component file created

---
+++ src/modules/auth/components/ButtonCardComponent.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps({
+  title: String,
+  img: String,
+});
+
+
+</script>
+
+<template>
+  <div>
+    <img src="" alt="" />
+    <span> {{title}} </span>
+  </div>
+</template>
