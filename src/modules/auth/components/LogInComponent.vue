350c42c10044ad7b38a13baeb1373f57c43307f0
Darin Aguilar <bromomentanium@gmail.com>
2024-04-30 16:23:39 -0600

    chore: login component styles

--- src/modules/auth/components/LogInComponent.vue
+++ src/modules/auth/components/LogInComponent.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-
 //interfaces
 import type { logInType } from "@/typings/interfaces/Auth/LogInType";
 
 //components
-import ButtonCardComponent from "@/modules/auth/components/ButtonCardComponent.vue"
-
+import ButtonCardComponent from "@/modules/auth/components/ButtonCardComponent.vue";
 
 //native imports
 import { reactive } from "vue";
@@ -23,23 +21,53 @@ function submit() {
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col gap-[3rem] | sm:w-[18.75rem] | lg:w-[25rem]">
     <!-- title -->
-    <div>
-      <span> Inicia sesión </span>
+    <div class="text-center">
+      <span class="text-[1.5rem] | lg:text-[2.5rem]"> Inicia sesión </span>
     </div>
     <!-- end title -->
     <!-- form -->
-    <div>
-      <input type="text" name="name" id="name" v-model="form.name" />
-      <input type="password" name="password" id="password" v-model="form.password" />
-      <button type="submit" @click="submit">Add</button>
+    <div class="flex flex-col gap-[3rem]">
+      <div class="block group">
+        <div class="flex gap-[0.6rem] p-[0.1rem]">
+          <img class="block" src="/auth/user.svg" alt="" />
+          <span class="group-hover:translate-x-1 duration-500">Usuario</span>
+        </div>
+        <input
+          class="local-inset-shadow w-full"
+          type="text"
+          name="name"
+          id="name"
+          v-model="form.name"
+        />
+      </div>
+      <div class="block group">
+        <div class="flex gap-[0.6rem] p-[0.1rem]">
+          <img class="block" src="/auth/password.svg" alt="" />
+          <span class="group-hover:translate-x-1 duration-500">Contraseña</span>
+        </div>
+        <input
+          class="local-inset-shadow w-full"
+          type="password"
+          name="password"
+          id="password"
+          v-model="form.password"
+        />
+      </div>
+      <button class="local-shadow bg-[#15395A]" type="submit" @click="submit">
+        Ingresar
+      </button>
     </div>
     <!-- end form -->
     <!-- link buttons -->
-    <div>
-      <ButtonCardComponent title="regístrate" img="" />
-      <ButtonCardComponent title="Modo invitado" img="" />
+    <div class="flex w-full justify-around pt-5">
+      <ButtonCardComponent
+        title="regístrate"
+        img="/auth/signup.svg"
+        path="/sesion/registro"
+      />
+      <ButtonCardComponent title="Modo invitado" img="/auth/guest.svg" path="" />
     </div>
     <!-- link buttons -->
   </div>
