0ca3750ce107c189888389c2e179930bad93c931
Darin Aguilar <bromomentanium@gmail.com>
2024-04-30 16:23:19 -0600

    chore: button component styles

--- src/modules/auth/components/ButtonCardComponent.vue
+++ src/modules/auth/components/ButtonCardComponent.vue
@@ -1,15 +1,30 @@
 <script lang="ts" setup>
+//props
 defineProps({
-  title: String,
-  img: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
 });
-
-
 </script>
 
 <template>
-  <div>
-    <img src="" alt="" />
-    <span> {{title}} </span>
-  </div>
+  <RouterLink :to="path">
+    <button class="flex flex-col justify-center items-center h-[70px] group">
+      <img
+        class="h-[50px] object-cover block group-hover:-translate-y-1 duration-500"
+        :src="img"
+        alt=""
+      />
+      <span class="text-center"> {{ title }} </span>
+    </button>
+  </RouterLink>
 </template>
