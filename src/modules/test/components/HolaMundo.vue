c059b9f78b38bcd8f2a5317ccfb3f50723ab9cab
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-29 20:01:11 -0600

    chore: fixed app view

--- src/modules/test/components/HolaMundo.vue
+++ src/modules/test/components/HolaMundo.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="w-full">
+  <div class="flex flex-col items-center justify-center w-full min-h-screen">
+    <img alt="Interiano logo" src="/interiano-logo-shadowed.svg"/>
     <h1 class="text-center">¡Hola mundo!</h1>
   </div>
 </template> 
