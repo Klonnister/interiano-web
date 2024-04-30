03f9ba2e0438db1dbebe41ee06ef3473178cac92
Darin Aguilar <bromomentanium@gmail.com>
2024-04-30 16:24:10 -0600

    chore: signup component style

--- src/modules/auth/components/SignupComponent.vue
+++ src/modules/auth/components/SignupComponent.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
+//interfaces
 import type { singUpType } from "@/typings/interfaces/Auth/SingUpType";
+
+//native imports
 import { reactive } from "vue";
 
 //variables
@@ -16,24 +19,64 @@ function submit() {
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col gap-[3rem] | sm:w-[18.75rem] | lg:w-[25rem]">
     <!-- title -->
-    <div>
-      <span>Regístrate</span>
+    <div class="text-center">
+      <span class="text-[1.5rem] | lg:text-[2.5rem]"> Regístrate </span>
     </div>
     <!-- end title -->
     <!-- form -->
-    <div>
-      <input type="text" name="name" id="name"  v-model="form.name">
-      <input type="password" name="password" id="password" v-model="form.password">
-      <input type="password" name="passwordConfirm" id="passwordConfirm" v-model="form.passwordConfirm">
-      <button type="submit" @click="submit ">Registrarse</button>
+    <div class="flex flex-col gap-[3rem]">
+      <div class="block group">
+        <div class="flex gap-[0.6rem] p-[0.1rem]">
+          <img class="block" src="/auth/user.svg" alt="" />
+          <span class="group-hover:translate-x-1 duration-500">Nombre de usuario</span>
+        </div>
+        <input
+          class="local-inset-shadow w-full"
+          type="text"
+          name="createName"
+          id="createName"
+          v-model="form.name"
+        />
+      </div>
+      <div class="block group">
+        <div class="flex gap-[0.6rem] p-[0.1rem]">
+          <img class="block" src="/auth/password.svg" alt="" />
+          <span class="group-hover:translate-x-1 duration-500">crear Contraseña</span>
+        </div>
+        <input
+          class="local-inset-shadow w-full"
+          type="password"
+          name="createPassword"
+          id="createPassword"
+          v-model="form.password"
+        />
+      </div>
+      <div class="block group">
+        <div class="flex gap-[0.6rem] p-[0.1rem]">
+          <img class="block" src="/auth/password.svg" alt="" />
+          <span class="group-hover:translate-x-1 duration-500">Confirmar contraseña</span>
+        </div>
+        <input
+          class="local-inset-shadow w-full"
+          type="password"
+          name="createPasswordConfirm"
+          id="createPasswordConfirm"
+          v-model="form.passwordConfirm"
+        />
+      </div>
+      <button class="local-shadow bg-[#15395A]" type="submit" @click="submit">
+        Ingresar
+      </button>
     </div>
     <!-- end form -->
     <!-- link buttons -->
-    <div>
-      <button type="submit" >Volver</button>
-    </div>
-     <!-- end link buttons -->
+    <RouterLink to="/sesion/inicio">
+      <div class="text-center underline">
+        <span>Volver</span>
+      </div>
+    </RouterLink>
+    <!-- link buttons -->
   </div>
 </template>
