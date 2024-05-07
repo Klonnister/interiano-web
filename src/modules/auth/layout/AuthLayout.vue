89a0c076167323cc9183cfbda924671330d79250
Darin Aguilar <bromomentanium@gmail.com>
2024-05-06 22:02:37 -0600

    chore: auth better coms

--- src/modules/auth/layout/AuthLayout.vue
+++ src/modules/auth/layout/AuthLayout.vue
@@ -3,6 +3,7 @@
   <div
     class="flex flex-col container mx-auto flex min-w-full min-h-screen items-center justify-center | lg:flex-row lg-justify-around"
   >
+  <!-- layout -->
     <div class="flex justify-center p-10 | lg:w-[50%]">
       <div
         class="flex flex-col gap-8 text-center w-[13.5rem] | sm:w-[18.75rem] | lg:w-[20rem] | xl:w-[21rem] | 2xl:w-[23rem]"
@@ -18,9 +19,11 @@
         </p>
       </div>
     </div>
+    <!-- layout -->
     <div
       class="hidden | lg:block h-[30rem] min-h-[1em] w-px bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400"
     ></div>
+    <!-- view component -->
     <div class="lg:w-[50%] items-center justify-center flex">
       <router-view v-slot="{ Component }">
         <transition name="fade" mode="out-in">
@@ -28,6 +31,7 @@
         </transition>
       </router-view>
     </div>
+        <!-- view component -->
   </div>
 </template>
 
