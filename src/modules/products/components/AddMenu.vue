d839099f311654fbd8ec0bc246361bd0ecd716a6
Dennis Herrera <dennishumher25@hotmail.com>
2024-05-10 20:18:13 -0600

    chore: fixed add products menu

--- src/modules/products/components/AddMenu.vue
+++ src/modules/products/components/AddMenu.vue
@@ -16,33 +16,46 @@ const { width } = useWindowSize();
     :reserve-scroll-bar-gap="true"
   > 
     <div
-      class="fixed top-0 right-0 px-8 py-6 bg-[#0E2032] h-max w-full sm:w-max sm:min-h-screen"
-    >
-      <button @click="emit('close')" class="fixed right-6">
-        <Icon
-          icon="iconamoon:close-bold"
-          class="w-7 h-7 text-[#D0D9F6]"
-        />
-      </button>
-      <p class="text-2xl text-center mb-4">Menú agregar</p>
+      class="fixed top-0 right-0 bg-[#0E2032] h-max w-full sm:w-[22rem] 2xl:w-[25rem] sm:min-h-screen"
+    > 
+      <div class="bg-[#09141F] px-8 py-6">
+        <button @click="emit('close')" class="fixed right-6">
+          <Icon
+            icon="iconamoon:close-bold"
+            class="w-7 h-7 text-[#D0D9F6]"
+          />
+        </button>
+        <p class="text-2xl text-center sm:text-start">Menú agregar</p>
+      </div>
       
-      <!-- <div
-        class="flex flex-col local-link-borders"
-        v-if="layoutStore.addMenu.links.length"
-      >
+      <div class="flex flex-col local-link-borders px-8 pt-2 pb-4">
         <RouterLink
-          v-for="link in layoutStore.addMenu.links"
-          :key="link.name"
-          :to="link.path"
-          class="flex gap-3 items-center justify-center sm:justify-start py-6 px-6 sm:px- hover:scale-105 transition-all duration-300 ease-in-out max-w-80 w-full mx-auto"
+          class="flex gap-3 items-center justify-center sm:justify-start py-6 px-4 sm:px- hover:scale-105 transition-all duration-300 ease-in-out max-w-80 w-full mx-auto"
+          :to="{ name: 'home' }"
         >
           <Icon
-            :icon="link.icon"
+            icon="mdi:add"
             class="w-6 h-6"
           />
-          <span>{{ link.name }}</span >
+          <span>Crear producto</span >
         </RouterLink>
-      </div>  -->
+
+        <button class="flex gap-3 items-center justify-center sm:justify-start py-6 px-4 sm:px- hover:scale-105 transition-all duration-300 ease-in-out max-w-80 w-full mx-auto">
+          <Icon
+            icon="mdi:file-plus"
+            class="w-6 h-6"
+          />
+          <span>Agregar página al catálogo</span >
+        </button>
+
+        <button class="flex gap-3 items-center justify-center sm:justify-start py-6 px-4 sm:px- hover:scale-105 transition-all duration-300 ease-in-out max-w-80 w-full mx-auto">
+          <Icon
+            icon="mdi:file-check"
+            class="w-6 h-6"
+          />
+          <span>Agregar todos al catálogo</span >
+        </button>
+      </div> 
     </div>
   </VueFinalModal>
 </template>
