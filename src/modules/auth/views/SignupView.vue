f723ec3308f76e7e1d5fc2f66b0087c9267c3b5e
Darin Aguilar <bromomentanium@gmail.com>
2024-05-16 15:42:46 -0600

    chore: signup inputs styles

--- src/modules/auth/views/SignupView.vue
+++ src/modules/auth/views/SignupView.vue
@@ -7,6 +7,7 @@ import type { singUpType } from "@/modules/auth/types/SingUpType";
 
 //native imports
 import { reactive } from "vue";
+import InputText from 'primevue/inputtext';
 
 //variables
 const form = reactive<singUpType>({
@@ -22,21 +23,20 @@ function submit() {
 </script>
 
 <template>
-  <div class="flex flex-col gap-[3rem] | sm:w-[18.75rem] | lg:w-[20rem] | xl:w-[21rem] | 2xl:w-[23rem]">
+  <div class="flex flex-col gap-8 w-full lg:max-w-80 2xl:max-w-96">
     <!-- title -->
     <div class="text-center">
       <span class="text-[1.5rem] | lg:text-[2.5rem]"> Regístrate </span>
     </div>
     <!-- end title -->
     <!-- form -->
-    <form @submit.prevent="submit" class="flex flex-col justify-center gap-[3rem] px-5">
+    <form @submit.prevent="submit" class="flex flex-col gap-8">
       <div class="block group">
-        <div class="flex gap-[0.6rem] p-[0.1rem] ">
+        <label class="flex gap-[0.6rem] p-[0.1rem] mb-1">
           <img class="block" src="/auth/user.svg" alt="" />
           <span class="group-hover:translate-x-1 duration-500">Nombre de usuario</span>
-        </div>
-        <input
-          class="local-inset-shadow w-full"
+        </label>
+        <InputText
           type="text"
           name="createName"
           id="createName"
@@ -44,12 +44,11 @@ function submit() {
         />
       </div>
       <div class="block group">
-        <div class="flex gap-[0.6rem] p-[0.1rem]">
+        <div class="flex gap-[0.6rem] p-[0.1rem] mb-1">
           <img class="block" src="/auth/password.svg" alt="" />
           <span class="group-hover:translate-x-1 duration-500">crear Contraseña</span>
         </div>
-        <input
-          class="local-inset-shadow w-full"
+        <InputText
           type="password"
           name="createPassword"
           id="createPassword"
@@ -57,19 +56,18 @@ function submit() {
         />
       </div>
       <div class="block group">
-        <div class="flex gap-[0.6rem] p-[0.1rem]">
+        <div class="flex gap-[0.6rem] p-[0.1rem] mb-1">
           <img class="block" src="/auth/password.svg" alt="" />
           <span class="group-hover:translate-x-1 duration-500">Confirmar contraseña</span>
         </div>
-        <input
-          class="local-inset-shadow w-full"
+        <InputText
           type="password"
           name="createPasswordConfirm"
           id="createPasswordConfirm"
           v-model="form.passwordconfirm"
         />
       </div>
-      <button class="local-shadow bg-[#15395A]" type="submit">Ingresar</button>
+      <button class="local-shadow bg-[#15395A] py-2 rounded-sm" type="submit">Ingresar</button>
     </form>
     <!-- end form -->
     <!-- link buttons -->
