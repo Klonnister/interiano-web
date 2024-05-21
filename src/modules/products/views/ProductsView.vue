11e14a28a98e2b2f8ded536fe255b450f738151c
Dennis Herrera <dennishumher25@hotmail.com>
2024-05-21 14:44:14 -0600

    chore: added paginator to ProductsView

--- src/modules/products/views/ProductsView.vue
+++ src/modules/products/views/ProductsView.vue
@@ -4,6 +4,7 @@ import type { Product, ProductResponse } from '../../shared/types/product.interf
 import { useFilterStore } from '@/modules/shared/stores/filterStore';
 import { apiRequest } from '@/modules/shared/helpers/api';
 import type { Category } from '@/modules/shared/types/category.interface';
+import Paginator from 'primevue/paginator';
 
 const products: Ref<Product[]> = ref([]);
 
@@ -27,16 +28,35 @@ setViewInfo();
 
 <template>
   <div class="flex flex-col gap-8">
-    <CardsViewHeader title="productos" :filters-button="true" add-button-type="button" />
+    <CardsViewHeader
+      title="productos"
+      :filters-button="true"
+      add-button-type="button"
+    />
 
-    <div class="w-full min-h-[30vh] lg:min-h-[65vh]">
-      <div class="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-x-5 gap-y-8 sm:gap-8 lg:gap-10 2xl:gap-14 min-h-96"
-        v-if="products.length">
-        <ProductCard v-for="product in products" :product="product" :key="product.id" />
+    <div class="w-full min-h-[40vh] lg:min-h-[65vh]">
+      <div v-if="products.length" class="w-full">
+        <div class="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-x-5 gap-y-8 sm:gap-8 lg:gap-10 2xl:gap-14 min-h-96">
+          <ProductCard
+            v-for="product in products"
+            :product="product"
+            :key="product.id"
+          />
+        </div>
+        <Paginator
+          :template="{
+              '640px': 'FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink',
+              default: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink'
+          }"
+          current-page-report-template="({currentPage} de {totalPages})"
+          :rows="12"
+          :totalRecords="60"
+          class="w-max mx-auto mt-10"
+        />
       </div>
 
-      <div class="min-h-[40vh] lg:min-h-[65vh] flex justify-center items-center" v-else>
-        <p class="lg:text-xl opacity-70">No hay productos para mostrar</p>
+      <div v-else class="min-h-[40vh] lg:min-h-[65vh] flex justify-center items-center w-full">
+        <p class="lg:text-xl 2xl:text-2xl opacity-70">No hay productos para mostrar</p>
       </div>
     </div>
   </div>
